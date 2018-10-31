e1f70ab101c4c46c5a9a1343d88da99146afa5dc
jon <kaz00@vivaldi.net>
2018-11-01 01:01:29 +0200

    New bash script drawAterminal.sh

---
+++ drawAterminal.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+slopoutput=$(slop -f "%x %y %w %h")
+X=$(echo $slopoutput | awk '{print $1}')
+Y=$(echo $slopoutput | awk '{print $2}')
+W=$(($(echo $slopoutput | awk '{print $3}')/8))
+H=$(($(echo $slopoutput | awk '{print $4}')/16))
+#urxvt -g ${W}x${H}+${X}+${Y}
+xfce4-terminal --geometry ${W}x${H}+${X}+${Y}
