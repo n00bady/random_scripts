d2a00286dec7e18b4cc43183efbbdbab04bdf9eb
n00bady <n00bady@firemail.cc>
2022-09-20 19:30:53 +0300

    Added the path to the TTC addon as a variable also changed it for my new installation.

--- ttcfetch.sh
+++ ttcfetch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Here you put the path to your TamrielTradeCentre addon
+TTC_Path="/mnt/d6f352a2-5fc3-42ed-8bef-e465e7933828/SSD_SteamLibrary/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/My Documents/Elder Scrolls Online/live/AddOns/TamrielTradeCentre/"
+
 echo -e "\e[35m================================================================================\e[0m"
 
 echo -e "\e[32mChanging to Downloads folder...\e[0m"
@@ -10,7 +13,7 @@ wget https://eu.tamrieltradecentre.com/download/PriceTable &&
 
 # The destination path changes depending on how you have ESO installed !!!
 echo -e "\e[32mUnziping the PriceTable and moving it to TamrielTradeCentre addon folder...\e[0m"
-unzip -o PriceTable -d "/run/media/jon/SSD1/SSD_SteamLibrary/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/My Documents/Elder Scrolls Online/live/AddOns/TamrielTradeCentre/" &&
+unzip -o PriceTable -d "$TTC_Path" &&
 
 echo -e "\n\e[32mDeleting the Pricetable file from Downloads...\e[0m"
 rm $HOME/Downloads/PriceTable &&
