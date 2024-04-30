f9d22403f92f32fcba422a65fc74c3ac9e6e83e1
n00bady <kaz00@vivaldi.net>
2024-04-30 23:46:31 +0300

    bash syntax so good...

--- hqgifconv.sh
+++ hqgifconv.sh
@@ -4,7 +4,7 @@
 # $2 is the name you want to give to the output
 # $3 the fps you want the output to have
 
-if [ $# < 3 ]; then
+if (($# < 3 )); then
     echo "Too few arguments!"
 else
     ffmpeg -i "$1" -vf palettegen "palette.png" &&
